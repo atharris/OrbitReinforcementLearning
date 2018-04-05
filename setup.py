530b44c08b47ab66cc7818d27363228d029cdac3
atharris <anha5550@colorado.edu>
2018-04-05 12:18:21 -0600

    Updating dependencies.

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_orbit',
       version='0.0.4',
-      install_requires=['gym','matplotlib','numpy','pandas']  # And any other dependencies foo needs
+      install_requires=['gym','matplotlib','numpy','pandas','scipy']  # And any other dependencies foo needs
 )
